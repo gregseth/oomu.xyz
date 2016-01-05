f6bbe1f7accc3e6ef74394f621cfd1feace8e744
Grégory Millasseau <greg.seth@gmail.com>
2016-01-05 12:38:45 +0100

    Adding day duration and delta fix for sur rise/set

--- index.php
+++ index.php
@@ -34,11 +34,15 @@ a:hover {
 require('suncalc.php');
 
 // Paris
-$latitude = 48.85;
-$longitude = 2.35;
+$latitude = 48.7941;
+$longitude = 2.2745;
 
 $SC = new SunCalc(new DateTime(), $latitude, $longitude);
-$SCyesterday = new SunCalc(new DateTime('yesterday'), $latitude, $longitude);
+$SCyesterday = new SunCalc(
+    (new DateTime())->sub(new DateInterval('P1D')),
+    $latitude, 
+    $longitude
+);
 
 $sun = $SC->getSunTimes();
 $suny = $SCyesterday->getSunTimes();
@@ -50,12 +54,39 @@ $ph = round($SC->getMoonIllumination()['phase'], 2);
 $tick = str_pad('^', $ph*strlen($phases)+1, ' ', STR_PAD_LEFT);
 
 function fmtEphemLine($data, $key, $diff=null) {
-    $suffix = ' '.$data[$key]->format('H:i');
+    $suffix = ' '.$data[$key]->format('H:i:s');
     if ($diff != null) {
-        $suffix .= ' '.$diff[$key]->diff($data[$key])->format('%R%i\'%S"').'';
-        // $suffix .= ' ('.$diff[$key]->format('H.i').')';
+        $todaytime = $data[$key];
+        $difftime = clone $diff[$key];
+        $difftime->add(new DateInterval('P1D'));
+        //echo $todaytime->format(DateTime::ISO8601)."\n";
+        //echo $difftime->format(DateTime::ISO8601)."\n";
+        $suffix .= ' '.$difftime->diff($todaytime)->format('%R%i\'%S"').'';
+        //$suffix .= ' ('.$difftime->format('H.i.s').')';
     }
     return str_pad(ucfirst($key).' ', 80-strlen($suffix), '.').$suffix."\n";
+}
+function fmtDaytimeLine() {
+    global $sun, $suny;
+    $todayduration = $sun['sunrise']->diff($sun['sunset']);
+    $suffix = ' '.$todayduration->format('%h.%I\'%S"');
+
+    $yesterdayduration = $suny['sunrise']->diff($suny['sunset']);
+    $deltasecs = $todayduration->h *60*60 + $todayduration->i *60 + $todayduration->s
+        - ($yesterdayduration->h *60*60 + $yesterdayduration->i *60 + $yesterdayduration->s);
+
+    $d1 = new DateTime('@0');
+    $d2 = clone $d1;
+    $i = new DateInterval('PT'.abs($deltasecs).'S');
+    if ($deltasecs > 0) {
+        $d2->sub($i);
+    } else {
+        $d2->add($i);
+    }
+    
+    $suffix .= ' '.$d2->diff($d1)->format('%R%i\'%S"').'';
+
+    return str_pad('Daytime ', 80-strlen($suffix), '.').$suffix."\n";
 }
 function fmtLat($lat) { return abs($lat).($lat>0 ? 'N' : 'S'); }
 function fmtLon($lon) { return abs($lon).($lon>0 ? 'E' : 'W'); }
@@ -88,6 +119,7 @@ $$$,     $$$$$$,     $$$$$$$$$$$"$$$ $$      $$$            Y$$$P         c$$"  
     * <?=fmtEphemLine($sun, 'sunrise', $suny)?>
     * <?=fmtEphemLine($sun, 'sunset', $suny)?>
     * <?=fmtEphemLine($sun, 'dusk', $suny)?>
+    * <?=fmtDaytimeLine()?>
     
     * <?=fmtEphemLine($moon, 'moonrise')?>
     * <?=fmtEphemLine($moon, 'moonset')?>
